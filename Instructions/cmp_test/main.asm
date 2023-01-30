7de20dbbe893bf42c42992213793c89dce471719
PhuwadonV <phuwadonpeak@hotmail.com>
2023-01-31 01:36:02 +0700

    Add instruction cmp_test

---
+++ Instructions/cmp_test/main.asm
@@ -0,0 +1,66 @@
+includelib msvcrt.lib
+includelib legacy_stdio_definitions.lib
+printf proto
+
+.const
+separator db 30 dup("-"), 0Ah, 0
+format db 2 dup("%08x "), 0Ah, 0
+
+.code
+main proc
+    sub rsp, 40
+  ; ------------------------------
+
+    mov eax, 1
+
+    pushfq
+    cmp eax, 1
+
+    pushfq
+    mov rdx, [rsp]
+    popfq
+    popfq
+
+    pushfq
+    sub eax, 1
+
+    pushfq
+    mov r8, [rsp]
+    popfq
+    popfq
+
+    mov rcx, offset format
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    mov eax, 1
+
+    pushfq
+    test eax, 1
+
+    pushfq
+    mov rdx, [rsp]
+    popfq
+    popfq
+
+    pushfq
+    and eax, 1
+
+    pushfq
+    mov r8, [rsp]
+    popfq
+    popfq
+
+    mov rcx, offset format
+    call printf
+
+  ; ------------------------------
+    add rsp, 40
+    ret
+main endp
+
+end
