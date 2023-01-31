83fc6f0bb59070c8319fff9c488598fef60518a5
PhuwadonV <phuwadonpeak@hotmail.com>
2023-01-31 23:08:02 +0700

    Update instruction cmpxchg_xchg_xadd_pause

--- Instructions/cmpxchg_xchg_xadd_pause/main.asm
+++ Instructions/cmpxchg_xchg_xadd_pause/main.asm
@@ -3,85 +3,100 @@ includelib legacy_stdio_definitions.lib
 printf proto
 CreateThread proto
 Sleep proto
-WaitForSingleObject proto
+WaitForMultipleObjects proto
 
 .const
 separator db 30 dup("-"), 0Ah, 0
-format1 db "Main   : ", 2 dup("%d "), 0Ah, 0
-format2 db "Thread : ", 2 dup("%d "), 0Ah, 0
-format3 db "Main: ", 0Ah, 0
-format4 db "Thread", 0Ah, 0
+format db "%d", 0Ah, 0
 
 .data
-dwThreadId1 dd ?
-dwThreadId2 dd ?
-
 align 16
 @lock dd 4 dup(0h)
-@value1 dd 4 dup(0h)
-@value2 dd 4 dup(0h)
+sum dd 4 dup(?)
 
 .code
 main proc
-    push rbx
-    sub rsp, 32
+    sub rsp, 56
   ; ------------------------------
 
-    mov rcx, thread1
+    mov [sum], 0
+
+    mov rcx, thread_cmpxchg
+    mov rdx, 500000
     call create_thread
-    mov rbx, rax
+    mov [rsp + 32], rax
 
-    mov rcx, 10
-    call Sleep
+    mov rcx, thread_cmpxchg
+    mov rdx, 500000
+    call create_thread
+    mov [rsp + 40], rax
 
-    xor eax, eax
-    align 16
-@@:
-    pause
-    lea edx, [eax + 1]
-    lock cmpxchg [@value1], edx
-    jne @b
+    mov rcx, 2
+    lea rdx, [rsp + 32]
+    mov r8, 1
+    mov r9d, 0FFFFFFFFh
+    call WaitForMultipleObjects
 
-    mov rcx, offset format1
-    mov edx, eax
-    mov r8d, [@value1]
+    mov rcx, offset format
+    mov edx, [sum]
     call printf
-
-    mov rcx, rbx
-    mov rdx, 0FFFFFFFFh
-    call WaitForSingleObject
 
   ; ------------------------------
     mov rcx, offset separator
     call printf
   ; ------------------------------
 
-    mov [@lock], 1
+    mov [sum], 0
 
-    mov rcx, thread2
+    mov rcx, thread_xchg
+    mov rdx, 500000
     call create_thread
-    mov rbx, rax
+    mov [rsp + 32], rax
 
-spin_lock:
-    mov eax, [@lock]
-    cmp [@lock], 0
-    je get_lock
-    pause
-    jmp spin_lock
-get_lock:
-    mov eax, 1
-    xchg [@lock], eax
-    cmp eax, 0
-    jne spin_lock
+    mov rcx, thread_xchg
+    mov rdx, 500000
+    call create_thread
+    mov [rsp + 40], rax
 
-    mov rcx, offset format3
+    mov rcx, 2
+    lea rdx, [rsp + 32]
+    mov r8, 1
+    mov r9d, 0FFFFFFFFh
+    call WaitForMultipleObjects
+
+    mov rcx, offset format
+    mov edx, [sum]
     call printf
 
-    mov [@lock], 0
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    mov [sum], 0
+
+    mov rcx, thread_xadd
+    mov rdx, 500000
+    call create_thread
+    mov [rsp + 32], rax
+
+    mov rcx, thread_xadd
+    mov rdx, 500000
+    call create_thread
+    mov [rsp + 40], rax
+
+    mov rcx, 2
+    lea rdx, [rsp + 32]
+    mov r8, 1
+    mov r9d, 0FFFFFFFFh
+    call WaitForMultipleObjects
+
+    mov rcx, offset format
+    mov edx, [sum]
+    call printf
 
   ; ------------------------------
-    add rsp, 32
-    pop rbx
+    add rsp, 56
     xor eax, eax
     ret
 main endp
@@ -91,11 +106,11 @@ create_thread proc
   ; ------------------------------
 
     mov r8, rcx
+    mov r9, rdx
     lea rax, [rsp + 48]
 
     xor ecx, ecx
     xor edx, edx
-    xor r9, r9
     mov [rsp + 32], ecx
     mov [rsp + 40], rax
     call CreateThread
@@ -105,49 +120,79 @@ create_thread proc
     ret
 create_thread endp
 
-thread1 proc
+thread_cmpxchg proc
     sub rsp, 40
   ; ------------------------------
     
-    mov ecx, dword ptr [@lock]
-    mov edx, 1
-
     align 16
 @@:
-    mov eax, 1
-    lock xadd dword ptr [@lock], eax
     pause
-    cmp eax, ecx
-    adcx ecx, edx
-    je @b
-
-    mov rcx, offset format2
-    mov edx, eax
-    mov r8d, dword ptr [@lock]
-    call printf
+    mov eax, [sum]
+    lea edx, [eax + 1]
+    lock cmpxchg [sum], edx
+    jne @b
+    dec ecx
+    test ecx, ecx
+    jnz @b
 
   ; ------------------------------
     add rsp, 40
     xor eax, eax
     ret
-thread1 endp
+thread_cmpxchg endp
 
-thread2 proc
+thread_xchg proc
     sub rsp, 40
   ; ------------------------------
 
-    mov rcx, offset format4
-    call printf
+    align 16
+spin_lock:
+    mov eax, [@lock]
+    cmp [@lock], 0
+    je get_lock
+    pause
+    jmp spin_lock
+get_lock:
+    mov eax, 1
+    xchg [@lock], eax
+  ; mov eax, [@lock]
+  ; mov [@lock], 1
+    cmp eax, 0
+    jne spin_lock
 
-    mov ecx, 1000
-    call Sleep
-
+    add [sum], 1
     mov [@lock], 0
+    
+    mfence
+    lfence
+
+    dec ecx
+    test ecx, ecx
+    jnz spin_lock
 
   ; ------------------------------
     add rsp, 40
     xor eax, eax
     ret
-thread2 endp
+thread_xchg endp
+
+thread_xadd proc
+    sub rsp, 40
+  ; ------------------------------
+    
+    align 16
+@@:
+    pause
+    mov eax, 1
+    lock xadd [sum], eax
+    dec ecx
+    test ecx, ecx
+    jnz @b
+
+  ; ------------------------------
+    add rsp, 40
+    xor eax, eax
+    ret
+thread_xadd endp
 
 end
