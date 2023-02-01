c646b96faa5efb103aa4aafb9fba105f19f2a706
PhuwadonV <phuwadonpeak@hotmail.com>
2023-02-01 18:30:03 +0700

    Add instruction divss_divsd

---
+++ Instructions/divss_divsd/main.asm
@@ -0,0 +1,42 @@
+includelib msvcrt.lib
+includelib legacy_stdio_definitions.lib
+printf proto
+
+.const
+separator db 30 dup("-"), 0Ah, 0
+format db "%f", 0Ah, 00
+
+.code
+main proc
+    sub rsp, 40
+  ; ------------------------------
+
+    mov eax, 40000000h ; 2.0f
+    movd xmm0, eax
+    divss xmm0, xmm0
+    cvtss2sd xmm0, xmm0
+
+    mov rcx, offset format
+    movd rdx, xmm0
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    mov rax, 4000000000000000h ; 2.0
+    movq xmm0, rax
+    divsd xmm0, xmm0
+
+    mov rcx, offset format
+    movd rdx, xmm0
+    call printf
+
+  ; ------------------------------
+    add rsp, 40
+    xor eax, eax
+    ret
+main endp
+
+end
