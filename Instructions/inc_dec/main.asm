0a30e0077f34b9f0406daad1391c0d468538ccc3
PhuwadonV <phuwadonpeak@hotmail.com>
2023-02-01 18:12:06 +0700

    Add instruction inc_dec

---
+++ Instructions/inc_dec/main.asm
@@ -0,0 +1,36 @@
+includelib msvcrt.lib
+includelib legacy_stdio_definitions.lib
+printf proto
+
+.const
+separator db 30 dup("-"), 0Ah, 0
+format db "%d", 0Ah, 0
+
+.code
+main proc
+    sub rsp, 40
+  ; ------------------------------
+
+    xor edx, edx
+    inc edx
+
+    mov rcx, offset format
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    xor edx, edx
+    dec edx
+
+    mov rcx, offset format
+    call printf
+
+  ; ------------------------------
+    add rsp, 40
+    ret
+main endp
+
+end
