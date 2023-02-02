c1ec2f2277f60e46013ea4c350f53cf91cf39edd
PhuwadonV <phuwadonpeak@hotmail.com>
2023-02-03 04:06:58 +0700

    Update instruction cpuid_lfence_sfence_mfence

--- Instructions/cpuid_lfence_sfence_mfence/main.asm
+++ Instructions/cpuid_lfence_sfence_mfence/main.asm
@@ -3,6 +3,7 @@ includelib legacy_stdio_definitions.lib
 printf proto
 CreateThread proto
 WaitForSingleObject proto
+WaitForMultipleObjects proto
 
 .const
 separator db 30 dup("-"), 0Ah, 0
@@ -12,12 +13,15 @@ format db "%lld", 0Ah, 0
 step dd 0
 
 align 16
+turn dd 4 dup(0)
+a_wants dd 4 dup(0)
+b_wants dd 4 dup(0)
 dst dd 4096 dup(0h)
 
 .code
 main proc
     push rbx
-    sub rsp, 32
+    sub rsp, 48
   ; ------------------------------
 
     mov eax, 3
@@ -89,9 +93,9 @@ main proc
     call printf
   ; ------------------------------
 
-    mov rcx, thread
+    mov rcx, thread_consumer
     call create_thread
-    mov rbx, rax
+    mov rdx, rax
 
     align 16
 @@:
@@ -111,12 +115,35 @@ main proc
     sfence
     mov [step], 2
 
-    mov rcx, rbx
+    mov rcx, rdx
     mov rdx, 0FFFFFFFFh
     call WaitForSingleObject
 
   ; ------------------------------
-    add rsp, 32
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    mov [step], 0
+
+    mov rcx, thread_a
+    mov rdx, 1000000
+    call create_thread
+    mov [rsp + 32], rax
+
+    mov rcx, thread_b
+    mov rdx, 1000000
+    call create_thread
+    mov [rsp + 40], rax
+
+    mov rcx, 2
+    lea rdx, [rsp + 32]
+    mov r8, 1
+    mov r9d, 0FFFFFFFFh
+    call WaitForMultipleObjects
+
+  ; ------------------------------
+    add rsp, 48
     pop rbx
     xor eax, eax
     ret
@@ -141,7 +168,7 @@ create_thread proc
     ret
 create_thread endp
 
-thread proc
+thread_consumer proc
     sub rsp, 40
   ; ------------------------------
     
@@ -160,7 +187,68 @@ thread proc
     add rsp, 40
     xor eax, eax
     ret
-thread endp
+thread_consumer endp
 
+thread_a proc
+    sub rsp, 40
+  ; ------------------------------
+    
+    align 16
+get_lock:
+    mov a_wants, 1
+    mov turn, 1
+    mfence
+
+    align 16
+@@:
+    mov eax, [b_wants]
+    mov edx, [turn]
+    cmp eax, 1
+    jne @f
+    cmp edx, 1
+    pause
+    je @b
+    mov a_wants, 0
+@@:
+    dec ecx
+    test ecx, ecx
+    jnz get_lock
+
+  ; ------------------------------
+    add rsp, 40
+    xor eax, eax
+    ret
+thread_a endp
+
+thread_b proc
+    sub rsp, 40
+  ; ------------------------------
+
+    align 16
+get_lock:
+    mov b_wants, 1
+    mov turn, 0
+    mfence
+
+    align 16
+@@:
+    mov eax, [a_wants]
+    mov edx, [turn]
+    cmp eax, 1
+    jne @f
+    cmp edx, 0
+    pause
+    je @b
+    mov b_wants, 0
+@@:
+    dec ecx
+    test ecx, ecx
+    jnz get_lock
+
+  ; ------------------------------
+    add rsp, 40
+    xor eax, eax
+    ret
+thread_b endp
 
 end
