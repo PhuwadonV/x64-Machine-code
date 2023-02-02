5a28085117d37d3afac8e2ed576e548049930dfc
PhuwadonV <phuwadonpeak@hotmail.com>
2023-02-02 15:33:17 +0700

    Add instruction lea

---
+++ Instructions/lea/main.asm
@@ -0,0 +1,31 @@
+includelib msvcrt.lib
+includelib legacy_stdio_definitions.lib
+printf proto
+
+.const
+format db 4 dup("%d "), 0Ah, 0
+
+.code
+main proc
+    sub rsp, 40
+  ; ------------------------------
+
+    mov ebx, 1000
+    mov ecx, 2
+
+    lea edx, [ebx - 100]
+    lea r8d, [ebx + ecx]
+    lea r9d, [ebx * 2]
+    lea ecx, [ebx + ecx * 4 + 100]
+
+    mov [rsp + 32], ecx
+
+    mov rcx, offset format
+    call printf
+
+  ; ------------------------------
+    add rsp, 40
+    ret
+main endp
+
+end
