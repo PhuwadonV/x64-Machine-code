39492cb79d9121f422d5d9bdc29462885a610c3f
PhuwadonV <phuwadonpeak@hotmail.com>
2023-02-03 12:55:25 +0700

    Add instruction Utils

---
+++ Instructions/Utils/utils.asm
@@ -0,0 +1,23 @@
+CreateThread proto
+
+.code
+create_thread proc
+    sub rsp, 56
+  ; ------------------------------
+
+    mov r8, rcx
+    mov r9, rdx
+    lea rax, [rsp + 48]
+
+    xor ecx, ecx
+    xor edx, edx
+    mov [rsp + 32], ecx
+    mov [rsp + 40], rax
+    call CreateThread
+
+  ; ------------------------------
+    add rsp, 56
+    ret
+create_thread endp
+
+end
