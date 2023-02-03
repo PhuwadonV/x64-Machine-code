cd8554c3206c7d2c0e41f16c70ba86aba239c133
PhuwadonV <phuwadonpeak@hotmail.com>
2023-02-03 14:57:47 +0700

    Add instruction xorps_xorpd

---
+++ Instructions/xorps_xorpd/main.asm
@@ -0,0 +1,48 @@
+includelib msvcrt.lib
+includelib legacy_stdio_definitions.lib
+printf proto
+
+.const
+separator db 30 dup("-"), 0Ah, 0
+format db 4 dup("%08x "), 0Ah, 0
+
+align 16
+src db 16 dup(0FFh)
+
+.code
+main proc
+    sub rsp, 32 + 8
+  ; ------------------------------
+
+    movaps xmm0, xmmword ptr [src]
+    xorps xmm0, xmm0
+    movups [rsp + 20], xmm0
+
+    mov rcx, offset format
+    mov edx, [rsp + 20]
+    mov r8d, [rsp + 24]
+    mov r9d, [rsp + 28]
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    movapd xmm0, xmmword ptr [src]
+    xorpd xmm0, xmm0
+    movupd [rsp + 20], xmm0
+
+    mov rcx, offset format
+    mov edx, [rsp + 20]
+    mov r8d, [rsp + 24]
+    mov r9d, [rsp + 28]
+    call printf
+
+  ; ------------------------------
+    add rsp, 32 + 8
+    xor eax, eax
+    ret
+main endp
+
+end
