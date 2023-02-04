b5a8ec1f2380489ed53bdcad5fca756681edc999
PhuwadonV <phuwadonpeak@hotmail.com>
2023-02-04 20:29:28 +0700

    Add instruction pause

---
+++ Instructions/pause/main.asm
@@ -0,0 +1,53 @@
+includelib msvcrt.lib
+includelib legacy_stdio_definitions.lib
+GetCurrentThread proto
+SetThreadAffinityMask proto
+printf proto
+
+.const
+format db "%lld", 0Ah, 0
+src dd 0h
+
+.code
+main proc
+    sub rsp, 32 + 8
+
+    call GetCurrentThread
+
+    mov dword ptr [rsp], 0
+
+    mov rcx, rax
+    mov rdx, [rsp]
+    call SetThreadAffinityMask
+  ; ------------------------------
+
+    clflush [src]
+
+    xor eax, eax
+    cpuid
+
+    rdtsc
+    mov r8d, eax
+    mov r9d, edx
+
+    pause
+    lfence
+
+    rdtsc
+    shl r9, 20h
+    shl rdx, 20h
+    or r8, r9
+    or rax, rdx
+    sub rax, r8
+
+    mov rcx, offset format
+    mov rdx, rax
+    call printf
+
+  ; ------------------------------
+    add rsp, 32 + 8
+    xor eax, eax
+    ret
+main endp
+
+end
