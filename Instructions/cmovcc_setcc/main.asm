d288909e5021f216d02695f214d9d001bff9dd65
PhuwadonV <phuwadonpeak@hotmail.com>
2023-02-10 15:58:54 +0700

    Add instruction cmovcc_setcc

--- Instructions/cmovcc_setcc/main.asm
+++ Instructions/cmovcc_setcc/main.asm
@@ -3,7 +3,8 @@ includelib legacy_stdio_definitions.lib
 printf proto
 
 .const
-format db "%d", 0
+separator db 30 dup("-"), 0Ah, 0
+format db "%d", 0Ah, 0
 
 .code
 main proc
@@ -15,6 +16,20 @@ main proc
     mov eax, 1
     cmp eax, 1
     cmove edx, eax
+
+    mov rcx, offset format
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    xor edx, edx
+
+    mov eax, 1
+    cmp eax, 1
+    sete dl
 
     mov rcx, offset format
     call printf
