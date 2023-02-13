3dc4d5b308aede442b00144276bcbb713c5522b2
PhuwadonV <phuwadonpeak@hotmail.com>
2023-02-13 17:44:54 +0700

    Add instruction addps_addpd_subps_subpd

--- Instructions/addps_addpd_subps_subpd/main.asm
+++ Instructions/addps_addpd_subps_subpd/main.asm
@@ -10,6 +10,8 @@ format2 db 2 dup("%f "), 0Ah, 0
 align 16
 src1 dd 1.0f, 2.0f, 3.0f, 4.0f
 src2 dq 1.0, 2.0
+src3 dd -1.0f, -2.0f, -3.0f, -4.0f
+src4 dq -1.0, -2.0
 
 .code
 main proc
@@ -35,6 +37,39 @@ main proc
 
     movapd xmm0, [src2]
     addpd xmm0, xmm0
+    movupd [rsp + 8], xmm0
+
+    mov rcx, offset format2
+    mov rdx, [rsp + 8]
+    mov r8, [rsp + 16]
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    vmovaps xmm0, [src3]
+    vmovaps xmm1, [src1]
+    vsubps xmm0, xmm0, xmm1
+    vcvtps2pd ymm0, xmm0
+    vmovupd [rsp + 8], ymm0
+
+    mov rcx, offset format1
+    mov rdx, [rsp + 8]
+    mov r8, [rsp + 16]
+    mov r9, [rsp + 24]
+    vzeroupper
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    movapd xmm0, [src4]
+    movapd xmm1, [src2]
+    subpd xmm0, xmm1
     movupd [rsp + 8], xmm0
 
     mov rcx, offset format2
