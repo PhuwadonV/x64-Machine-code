7b63ad8175cc5e2189c8bdb911f358615fee97e6
PhuwadonV <phuwadonpeak@hotmail.com>
2023-02-13 18:52:37 +0700

    Add instruction por_orps_orpd

---
+++ Instructions/por_orps_orpd/main.asm
@@ -0,0 +1,67 @@
+includelib msvcrt.lib
+includelib legacy_stdio_definitions.lib
+printf proto
+
+.const
+separator db 30 dup("-"), 0Ah, 0
+format db 4 dup("%08x "), 0Ah, 0
+
+align 16
+src db 16 dup(0FFh)
+@mask db 8 dup(0FFh, 0h)
+
+.code
+main proc
+    sub rsp, 32 + 8
+  ; ------------------------------
+
+    movdqa xmm0, xmmword ptr [@mask]
+    movdqa xmm1, xmmword ptr [src]
+    por xmm0, xmm1
+    movdqu [rsp + 20], xmm0
+
+    mov rcx, offset format
+    mov edx, [rsp + 20]
+    mov r8d, [rsp + 24]
+    mov r9d, [rsp + 28]
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    movaps xmm0, xmmword ptr [@mask]
+    movaps xmm1, xmmword ptr [src]
+    orps xmm0, xmm1
+    movups [rsp + 20], xmm0
+
+    mov rcx, offset format
+    mov edx, [rsp + 20]
+    mov r8d, [rsp + 24]
+    mov r9d, [rsp + 28]
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    movapd xmm0, xmmword ptr [@mask]
+    movapd xmm1, xmmword ptr [src]
+    orpd xmm0, xmm1
+    movupd [rsp + 20], xmm0
+
+    mov rcx, offset format
+    mov edx, [rsp + 20]
+    mov r8d, [rsp + 24]
+    mov r9d, [rsp + 28]
+    call printf
+
+  ; ------------------------------
+    add rsp, 32 + 8
+    xor eax, eax
+    ret
+main endp
+
+end
