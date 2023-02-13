4ab85ee9e75b29871ca226b00b0929855d55a080
PhuwadonV <phuwadonpeak@hotmail.com>
2023-02-13 17:37:34 +0700

    Add instruction addss_addsd_subss_subsd

--- Instructions/addss_addsd_subss_subsd/main.asm
+++ Instructions/addss_addsd_subss_subsd/main.asm
@@ -34,6 +34,37 @@ main proc
     call printf
 
   ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    mov eax, 0BF800000h ; -1.0f
+    mov ecx, 3F800000h ; 1.0f
+    movd xmm0, eax
+    movd xmm1, ecx
+    subss xmm0, xmm1
+    cvtss2sd xmm0, xmm0
+
+    mov rcx, offset format
+    movq rdx, xmm0
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    mov rax, 0BFF0000000000000h ; -1.0
+    mov rcx, 3FF0000000000000h ; 1.0
+    movq xmm0, rax
+    movq xmm1, rcx
+    subsd xmm0, xmm1
+
+    mov rcx, offset format
+    movq rdx, xmm0
+    call printf
+
+  ; ------------------------------
     add rsp, 32 + 8
     xor eax, eax
     ret
