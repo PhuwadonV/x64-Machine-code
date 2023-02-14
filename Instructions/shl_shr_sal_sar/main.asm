46a433bf207e0d23834eb9f77d249c9644ffa024
PhuwadonV <phuwadonpeak@hotmail.com>
2023-02-14 15:22:53 +0700

    Update instruction shl_shr_sal_sar

--- Instructions/shl_shr_sal_sar/main.asm
+++ Instructions/shl_shr_sal_sar/main.asm
@@ -3,22 +3,34 @@ includelib legacy_stdio_definitions.lib
 printf proto
 
 .const
-format db 4 dup("%d "), 0
+separator db 30 dup("-"), 0Ah, 0
+format db 2 dup("%d "), 0Ah, 0
 
 .code
 main proc
     sub rsp, 32 + 8
   ; ------------------------------
 
-    mov edx, 2
-    mov r8d, 2
-    mov r9d, -4
-    mov eax, -4
+    mov edx, 4
+    mov r8d, 4
 
     shl edx, 1
     shr r8d, 1
-    sal r9d, 1
-    sar eax, 1
+
+    mov rcx, offset format
+    mov [rsp + 32], eax
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    mov edx, -4
+    mov r8d, -4
+
+    sal edx, 1
+    sar r8d, 1
 
     mov rcx, offset format
     mov [rsp + 32], eax
