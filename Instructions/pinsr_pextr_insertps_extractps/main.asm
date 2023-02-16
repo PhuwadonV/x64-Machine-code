a10b9934dd133a65d89ab22d2e19c3ef62a7afed
PhuwadonV <phuwadonpeak@hotmail.com>
2023-02-16 16:49:52 +0700

    Add instruction pinsr_pextr_insertps_extractps

--- Instructions/pinsr_pextr_insertps_extractps/main.asm
+++ Instructions/pinsr_pextr_insertps_extractps/main.asm
@@ -5,13 +5,15 @@ printf proto
 .const
 separator db 30 dup("-"), 0Ah, 0
 format1 db 4 dup("%08x "), 0Ah, 0
-format2 db 4 dup("%f "), 0Ah, 0
-format3 db "%f", 0Ah, 0
+format2 db "%016llx", 0Ah, 0
+format3 db 4 dup("%f "), 0Ah, 0
+format4 db "%f", 0Ah, 0
 
 align 16
 src1 db 16 dup(11h)
-src2 dd 1.0f, 2.0f, 3.0f, 4.0f
-src3 dq 1.0, 2.0
+src2 dq 1122334455667788h, 99AABBCCDDEEFFh
+src3 dd 1.0f, 2.0f, 3.0f, 4.0f
+src4 dq 1.0, 2.0
 
 .code
 main proc
@@ -82,12 +84,56 @@ main proc
     call printf
   ; ------------------------------
 
-    vmovaps xmm0, xmmword ptr [src2]
+    movdqa xmm0, xmmword ptr [src2]
+    pextrb edx, xmm0, 0
+
+    mov rcx, offset format2
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    movdqa xmm0, xmmword ptr [src2]
+    pextrw edx, xmm0, 0
+
+    mov rcx, offset format2
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    movdqa xmm0, xmmword ptr [src2]
+    pextrd edx, xmm0, 0
+
+    mov rcx, offset format2
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    movdqa xmm0, xmmword ptr [src2]
+    pextrq rdx, xmm0, 0
+
+    mov rcx, offset format2
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    vmovaps xmm0, xmmword ptr [src3]
     vinsertps xmm0, xmm0, xmm0, 80h
     vcvtps2pd ymm0, xmm0
     vmovupd [rsp + 8], ymm0
 
-    mov rcx, offset format2
+    mov rcx, offset format3
     mov rdx, [rsp + 8]
     mov r8, [rsp + 16]
     mov r9, [rsp + 24]
@@ -99,12 +145,12 @@ main proc
     call printf
   ; ------------------------------
 
-    movaps xmm0, xmmword ptr [src3]
+    movaps xmm0, xmmword ptr [src4]
     extractps edx, xmm0, 3h
     movd xmm0, edx
     cvtss2sd xmm0, xmm0
 
-    mov rcx, offset format3
+    mov rcx, offset format4
     movq rdx, xmm0
     call printf
 
