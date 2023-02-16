33da58daf8c5f73f8c2ffea6304765a7bd07d2a2
PhuwadonV <phuwadonpeak@hotmail.com>
2023-02-16 16:39:46 +0700

    Add instruction pinsr_insertps_extractps

---
+++ Instructions/pinsr_insertps_extractps/main.asm
@@ -0,0 +1,117 @@
+includelib msvcrt.lib
+includelib legacy_stdio_definitions.lib
+printf proto
+
+.const
+separator db 30 dup("-"), 0Ah, 0
+format1 db 4 dup("%08x "), 0Ah, 0
+format2 db 4 dup("%f "), 0Ah, 0
+format3 db "%f", 0Ah, 0
+
+align 16
+src1 db 16 dup(11h)
+src2 dd 1.0f, 2.0f, 3.0f, 4.0f
+src3 dq 1.0, 2.0
+
+.code
+main proc
+    sub rsp, 32 + 8
+  ; ------------------------------
+
+    movdqa xmm0, xmmword ptr [src1]
+    mov eax, 99h
+    pinsrb xmm0, eax, 0
+    movdqu [rsp + 20], xmm0
+
+    mov rcx, offset format1
+    mov edx, [rsp + 20]
+    mov r8d, [rsp + 24]
+    mov r9d, [rsp + 28]
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    movdqa xmm0, xmmword ptr [src1]
+    mov eax, 9999h
+    pinsrw xmm0, eax, 0
+    movdqu [rsp + 20], xmm0
+
+    mov rcx, offset format1
+    mov edx, [rsp + 20]
+    mov r8d, [rsp + 24]
+    mov r9d, [rsp + 28]
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    movdqa xmm0, xmmword ptr [src1]
+    mov eax, 99999999h
+    pinsrd xmm0, eax, 0
+    movdqu [rsp + 20], xmm0
+
+    mov rcx, offset format1
+    mov edx, [rsp + 20]
+    mov r8d, [rsp + 24]
+    mov r9d, [rsp + 28]
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    movdqa xmm0, xmmword ptr [src1]
+    mov rax, 9999999999999999h
+    pinsrq xmm0, rax, 0
+    movdqu [rsp + 20], xmm0
+
+    mov rcx, offset format1
+    mov edx, [rsp + 20]
+    mov r8d, [rsp + 24]
+    mov r9d, [rsp + 28]
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    vmovaps xmm0, xmmword ptr [src2]
+    vinsertps xmm0, xmm0, xmm0, 80h
+    vcvtps2pd ymm0, xmm0
+    vmovupd [rsp + 8], ymm0
+
+    mov rcx, offset format2
+    mov rdx, [rsp + 8]
+    mov r8, [rsp + 16]
+    mov r9, [rsp + 24]
+    vzeroupper
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    movaps xmm0, xmmword ptr [src3]
+    extractps edx, xmm0, 3h
+    movd xmm0, edx
+    cvtss2sd xmm0, xmm0
+
+    mov rcx, offset format3
+    movq rdx, xmm0
+    call printf
+
+  ; ------------------------------
+    add rsp, 32 + 8
+    xor eax, eax
+    ret
+main endp
+
+end
