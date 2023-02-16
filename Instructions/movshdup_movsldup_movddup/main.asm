38e9ab2f7b3adf9c5d0309d4f6a671997ec9ee6a
PhuwadonV <phuwadonpeak@hotmail.com>
2023-02-16 14:11:35 +0700

    Add instruction movshdup_movsldup_movddup

--- Instructions/movshdup_movsldup_movddup/main.asm
+++ Instructions/movshdup_movsldup_movddup/main.asm
@@ -4,22 +4,38 @@ printf proto
 
 .const
 separator db 30 dup("-"), 0Ah, 0
-format1 db 2 dup("%f "), 0Ah, 0
-format2 db 4 dup("%08x "), 0Ah, 0
+format1 db 4 dup("%08x "), 0Ah, 0
+format2 db 2 dup("%f "), 0Ah, 0
 
 align 16
-src dd 2 dup(99h, 1.0f)
+src1 dd 2 dup(99h, 1.0f)
+src2 dd 2 dup(2.0f, 99h)
 
 .code
 main proc
     sub rsp, 32 + 8
   ; ------------------------------
 
-    movaps xmm0, [src]
+    movaps xmm0, [src1]
     movshdup xmm0, xmm0
     movups [rsp + 20], xmm0
 
-    mov rcx, offset format2
+    mov rcx, offset format1
+    mov edx, [rsp + 20]
+    mov r8d, [rsp + 24]
+    mov r9d, [rsp + 28]
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    movaps xmm0, [src2]
+    movsldup xmm0, xmm0
+    movups [rsp + 20], xmm0
+
+    mov rcx, offset format1
     mov edx, [rsp + 20]
     mov r8d, [rsp + 24]
     mov r9d, [rsp + 28]
@@ -35,7 +51,7 @@ main proc
     movddup xmm0, xmm0
     movupd [rsp + 8], xmm0
 
-    mov rcx, offset format1
+    mov rcx, offset format2
     mov rdx, [rsp + 8]
     mov r8, [rsp + 16]
     call printf
