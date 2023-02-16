21769f9e54921688e2ff13d77e1928dc5e509356
PhuwadonV <phuwadonpeak@hotmail.com>
2023-02-16 17:14:10 +0700

    Add instruction packss_packus

---
+++ Instructions/packss_packus/main.asm
@@ -0,0 +1,79 @@
+includelib msvcrt.lib
+includelib legacy_stdio_definitions.lib
+printf proto
+
+.const
+separator db 30 dup("-"), 0Ah, 0
+format1 db 4 dup("%08x "), 0Ah, 0
+
+align 16
+src1 dw 1, 2, 3, 4, 5555h, 5555h, 5555h, 5555h
+src2 dd 1, 2, 55555555h, 55555555h
+
+.code
+main proc
+    sub rsp, 32 + 8
+  ; ------------------------------
+
+    movdqa xmm0, xmmword ptr [src1]
+    packsswb xmm0, xmm0
+    movdqu [rsp + 20], xmm0
+
+    mov rcx, offset format1
+    mov edx, [rsp + 20]
+    mov r8d, [rsp + 24]
+    mov r9d, [rsp + 28]
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    movdqa xmm0, xmmword ptr [src2]
+    packssdw xmm0, xmm0
+    movdqu [rsp + 20], xmm0
+
+    mov rcx, offset format1
+    mov edx, [rsp + 20]
+    mov r8d, [rsp + 24]
+    mov r9d, [rsp + 28]
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    movdqa xmm0, xmmword ptr [src1]
+    packuswb xmm0, xmm0
+    movdqu [rsp + 20], xmm0
+
+    mov rcx, offset format1
+    mov edx, [rsp + 20]
+    mov r8d, [rsp + 24]
+    mov r9d, [rsp + 28]
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    movdqa xmm0, xmmword ptr [src2]
+    packusdw xmm0, xmm0
+    movdqu [rsp + 20], xmm0
+
+    mov rcx, offset format1
+    mov edx, [rsp + 20]
+    mov r8d, [rsp + 24]
+    mov r9d, [rsp + 28]
+    call printf
+
+  ; ------------------------------
+    add rsp, 32 + 8
+    xor eax, eax
+    ret
+main endp
+
+end
