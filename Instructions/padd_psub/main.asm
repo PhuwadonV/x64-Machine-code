b7a3e1d317e3fe126a176b336ea6c23ea615a9c1
PhuwadonV <phuwadonpeak@hotmail.com>
2023-02-16 17:49:24 +0700

    Add instruction padd_psub

---
+++ Instructions/padd_psub/main.asm
@@ -0,0 +1,148 @@
+includelib msvcrt.lib
+includelib legacy_stdio_definitions.lib
+printf proto
+
+.const
+separator db 30 dup("-"), 0Ah, 0
+format1 db 4 dup("%08x "), 0Ah, 0
+format2 db 2 dup("%016llx "), 0Ah, 0
+
+align 16
+src1 db 4 dup(1, 2, 3, 4)
+src2 dw 2 dup(1, 2, 3, 4)
+src3 dd 1, 2, 3, 4
+src4 dq 1, 2
+src5 db 4 dup(-1, -2, -3, -4)
+src6 dw 2 dup(-1, -2, -3, -4)
+src7 dd -1, -2, -3, -4
+src8 dq -1, -2
+
+.code
+main proc
+    sub rsp, 32 + 8
+  ; ------------------------------
+
+    movdqa xmm0, xmmword ptr [src1]
+    paddb xmm0, xmm0
+    movdqu [rsp + 20], xmm0
+
+    mov rcx, offset format1
+    mov edx, [rsp + 20]
+    mov r8d, [rsp + 24]
+    mov r9d, [rsp + 28]
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    movdqa xmm0, xmmword ptr [src2]
+    paddw xmm0, xmm0
+    movdqu [rsp + 20], xmm0
+
+    mov rcx, offset format1
+    mov edx, [rsp + 20]
+    mov r8d, [rsp + 24]
+    mov r9d, [rsp + 28]
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    movdqa xmm0, xmmword ptr [src3]
+    paddd xmm0, xmm0
+    movdqu [rsp + 20], xmm0
+
+    mov rcx, offset format1
+    mov edx, [rsp + 20]
+    mov r8d, [rsp + 24]
+    mov r9d, [rsp + 28]
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    movdqa xmm0, xmmword ptr [src4]
+    paddq xmm0, xmm0
+    movupd [rsp + 8], xmm0
+
+    mov rcx, offset format2
+    mov rdx, [rsp + 8]
+    mov r8, [rsp + 16]
+    call printf
+
+   ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    movdqa xmm0, xmmword ptr [src5]
+    movdqa xmm1, xmmword ptr [src1]
+    psubb xmm0, xmm1
+    movdqu [rsp + 20], xmm0
+
+    mov rcx, offset format1
+    mov edx, [rsp + 20]
+    mov r8d, [rsp + 24]
+    mov r9d, [rsp + 28]
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    movdqa xmm0, xmmword ptr [src6]
+    movdqa xmm1, xmmword ptr [src2]
+    psubw xmm0, xmm1
+    movdqu [rsp + 20], xmm0
+
+    mov rcx, offset format1
+    mov edx, [rsp + 20]
+    mov r8d, [rsp + 24]
+    mov r9d, [rsp + 28]
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    movdqa xmm0, xmmword ptr [src7]
+    movdqa xmm1, xmmword ptr [src3]
+    psubd xmm0, xmm1
+    movdqu [rsp + 20], xmm0
+
+    mov rcx, offset format1
+    mov edx, [rsp + 20]
+    mov r8d, [rsp + 24]
+    mov r9d, [rsp + 28]
+    call printf
+
+  ; ------------------------------
+    mov rcx, offset separator
+    call printf
+  ; ------------------------------
+
+    movdqa xmm0, xmmword ptr [src8]
+    movdqa xmm1, xmmword ptr [src4]
+    psubq xmm0, xmm1
+    movupd [rsp + 8], xmm0
+
+    mov rcx, offset format2
+    mov rdx, [rsp + 8]
+    mov r8, [rsp + 16]
+    call printf
+  
+  ; ------------------------------
+    add rsp, 32 + 8
+    xor eax, eax
+    ret
+main endp
+
+end
