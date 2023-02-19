b667aebb2c3061c2c94aa37c55aacb191343890e
PhuwadonV <phuwadonpeak@hotmail.com>
2023-02-19 13:27:31 +0700

    Add instruction rcpss and rcpps

--- Instructions/rcpss/main.asm
+++ Instructions/rcpss/main.asm
@@ -3,27 +3,20 @@ includelib legacy_stdio_definitions.lib
 printf proto
 
 .const
-format db 4 dup("%f "), 0Ah, 0
-
-align 16
-src dd 1.0f, 2.0f, 3.0f, 4.0f
-
+format db "%f", 0Ah, 0
 
 .code
 main proc
     sub rsp, 32 + 8
   ; ------------------------------
 
-    vmovaps xmm0, [src]
-    vrcpps xmm0, xmm0
-    vcvtps2pd ymm0, xmm0
-    vmovupd [rsp + 8], ymm0
+    mov eax, 40000000h ; 2.0f
+    movd xmm0, eax
+    rcpss xmm0, xmm0
+    cvtss2sd xmm0, xmm0
 
     mov rcx, offset format
-    mov rdx, [rsp + 8]
-    mov r8, [rsp + 16]
-    mov r9, [rsp + 24]
-    vzeroupper
+    movq rdx, xmm0
     call printf
 
   ; ------------------------------
